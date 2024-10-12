7e2978837e9bc8cd7c323be183249abcc48af61e
RodrigoTobio <rodrigohtobio@gmail.com>
2024-10-12 00:44:16 +0000

    feat(scripts): modifico el script Punto_B.sh para la funcionalidad pedida

--- RTA_SCRIPT_Examen_20241012/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#lo hago en /Examenes-UTN porque lo pide en el directorio raiz.
 sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
---
+++ RTA_SCRIPT_Examen_20241012/Punto_B.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#creo las particiones
+sudo fdisk /dev/sdc <<FIN
+$(for i in {1..3}; do
+echo "n"
+echo "p"
+echo ""
+echo ""
+echo "+1G"
+done)
+n
+e
+
+
+$(for i in {1..6}; do
+echo "n"
+echo ""
+echo "+1G"
+done)
+n
+
+
+
+w
+FIN
+#formateo particiones
+for i in {1..3};do
+        sudo mkfs -t ext4 "/dev/sdc$i"
+done
+for i in {5..11};do
+       sudo mkfs -t ext4 "/dev/sdc$i"
+done
+
+#monto en sus respectivas carpetas
+sudo mount /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1
+sudo mount /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2
+sudo mount /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3
+sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1
+sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2
+sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3
+sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1
+sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2
+sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3
+sudo mount /dev/sdc11 /Examenes-UTN/profesores
+
+#montaje persistente
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc11 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo mount -a
